bac91db26eb049050a1824bad62dabc9a343e0e1
knezevic-dev <miraknezeviccc@gmail.com>
2024-08-06 08:09:09 +0200

    UI app setup with styled components part1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { styled, ThemeProvider } from "styled-components";
+import { theme } from "./styles/Theme";
+import {
+  ButtonText,
+  MainTitle,
+  SectionSubtitle,
+  SectionTitle,
+  SubText,
+  Text,
+} from "./components/ui/Typography";
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <MainTitle>Hello</MainTitle>
+      <SectionTitle>Hello</SectionTitle>
+      <SectionSubtitle>Hello</SectionSubtitle>
+      <Text>Hello</Text>
+      <SubText>Hello</SubText>
+      <ButtonText>Hello</ButtonText>
+    </ThemeProvider>
+  );
 }
 
 export default App;
