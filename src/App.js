243545af2f4601f61eb7154aade7fd5a18ccd242
knezevic-dev <miraknezeviccc@gmail.com>
2024-08-07 12:58:47 +0200

    part 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
-import { styled, ThemeProvider } from "styled-components";
-import { theme } from "./styles/Theme";
+import { ThemeProvider } from "styled-components";
+import {
+  Play,
+  SkipLeft,
+  Pause,
+  SkipRight,
+  Volume,
+  Music,
+  ArrowLeft,
+  ArrowRight,
+  Search,
+  Heart,
+  Users,
+} from "components/ui/icons";
 import {
   ButtonText,
   MainTitle,
@@ -7,17 +19,32 @@ import {
   SectionTitle,
   SubText,
   Text,
-} from "./components/ui/Typography";
+} from "components/ui/Typography";
+import { GlobalStyles } from "styles/Global";
+import { theme } from "styles/Theme";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyles />
       <MainTitle>Hello</MainTitle>
       <SectionTitle>Hello</SectionTitle>
       <SectionSubtitle>Hello</SectionSubtitle>
       <Text>Hello</Text>
       <SubText>Hello</SubText>
       <ButtonText>Hello</ButtonText>
+      <br />
+      <Play />
+      <SkipLeft />
+      <Pause />
+      <SkipRight />
+      <Volume />
+      <Music />
+      <ArrowLeft />
+      <ArrowRight />
+      <Search />
+      <Heart />
+      <Users />
     </ThemeProvider>
   );
 }
